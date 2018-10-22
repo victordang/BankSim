a23d6be733d3b2c2b520b193f59eda117456f2a1
Victor Dang <victordang15@gmail.com>
2018-10-22 01:36:59 -0400

    added waitForSufficientFunds & synchronized stuff

---
+++ src/edu/temple/cis/c3238/banksim/Account.java
@@ -0,0 +1,58 @@
+package edu.temple.cis.c3238.banksim;
+
+/**
+ * @author Cay Horstmann
+ * @author Modified by Paul Wolfgang
+ * @author Modified by Charles Wang
+ */
+public class Account {
+
+    private volatile int balance;
+    private final int id;
+    private Bank myBank;
+
+    public Account(Bank myBank, int id, int initialBalance) {
+        this.myBank = myBank;
+        this.id = id;
+        balance = initialBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+   
+    public synchronized void waitForSufficientFunds(int amount){
+        while (myBank.isOpen() && amount >= balance){
+            try{
+                wait();
+            }catch (InterruptedException ex) {}
+        }
+    }
+   
+    
+    public synchronized boolean withdraw(int amount) {
+        if (amount <= balance) {
+            int currentBalance = balance;
+            Thread.yield(); // Try to force collision
+            int newBalance = currentBalance - amount;
+            balance = newBalance;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public synchronized void deposit(int amount) {
+        int currentBalance = balance;
+        Thread.yield();   // Try to force collision
+        int newBalance = currentBalance + amount;
+        balance = newBalance;
+        notifyAll();
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Account[%d] balance %d", id, balance);
+    }
+}
